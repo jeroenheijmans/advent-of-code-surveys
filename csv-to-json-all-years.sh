3e20f07e45ae10e10e4d55845746c1addcef354e
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-22 23:26:05 +0100

    Improve grouping and answer pre-processing

---
+++ csv-to-json-all-years.sh
@@ -0,0 +1,7 @@
+node ./2018/csv-to-json.js
+node ./2019/csv-to-json.js
+node ./2020/csv-to-json.js
+node ./2021/csv-to-json.js
+node ./2022/csv-to-json.js
+node ./2023/csv-to-json.js
+node ./2024/csv-to-json.js
